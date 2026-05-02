45df02ef1cc53375e91d38294580296962160a55
jaobrabo123 <jp2904.lindo@gmail.com>
2026-05-02 00:35:59 -0300

    Aprendendo Spring Security

---
+++ src/main/java/com/azevedo/libraryapi/model/Usuario.java
@@ -0,0 +1,28 @@
+package com.azevedo.libraryapi.model;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String login;
+
+    private String senha;
+
+    @Type(ListArrayType.class)
+    @Column(columnDefinition = "varchar[]")
+    private List<String> roles;
+
+}
